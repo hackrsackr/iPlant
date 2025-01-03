1d0fc5578d4221834ceeee7230ab25a660d9d558
rob <rob.knighton10@gmail.com>
2025-01-03 17:50:05 -0500

    cleaned up bme280

--- picam/src/BME280.py
+++ picam/src/BME280.py
@@ -1,13 +1,6 @@
 import smbus2
 import bme280
 
-# Setup
-port = 1
-address = 0x76
-bus = smbus2.SMBus(port)
-calibration_params = bme280.load_calibration_params(bus, address)
-
-
 '''
 The sample method will take a single reading and return a
 compensated_reading object
@@ -22,27 +15,30 @@ The compensated_reading class has the following attributes
         data.humidity
 '''
 
-def getBmeData() -> object:
-    data: object = bme280.sample(bus, address, calibration_params)
+def getBmeData(addr: str, bus: object, params: object) -> object:
+    data: object = bme280.sample(bus, addr, params)
+
     return(data)
 
-def getTempAndHumidity() -> float:
-    data: object        = bme280.sample(bus, address, calibration_params)
+def getTempAndHumidity(addr: str, bus: object, params: object) -> float:
+    data: object        = bme280.sample(bus, addr, params)
     temperature: float  = data.temperature * 9/5 + 32
     humidity: float     = data.humidity
 
     return(temperature, humidity)
 
-
 def main() -> None:
-    temperature, humidity = getTempAndHumidity()
+    port: int = 1
+    address: str = 0x76
+    bus: object = smbus2.SMBus(port)
+    calibration_params: object = bme280.load_calibration_params(bus, address)
+    
+    temperature, humidity = getTempAndHumidity(addr=address, bus=bus, params=calibration_params)
+    
     print(
         f"Temperature: {temperature:.2f}\n"
         f"Humidity[%]: {humidity:.2f}"
           )
-
-
-
 
 if __name__== "__main__":
     main()
